4bd08da9246743467adf1f4f5c4e94428d2c1709
aavendan <aavendan@gmail.com>
2021-04-04 23:19:41 -0500

    actualizaciones por heroku

--- src/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-app.use(express.static(__dirname+'/dist/material-dashboard-angular2-master'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/material-dashboard-angular2-master/index.html'));
